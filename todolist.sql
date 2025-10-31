409f6fe29bcea1738a298fa4c0abb8cf38a1280d
Ubuntu <lpe@lpe.archidep4.ch>
2025-10-31 14:19:42 +0000

    Initial commit from local project

---
+++ todolist.sql
@@ -0,0 +1,19 @@
+-- Active: 1760012767988@@127.0.0.1@3306@mysql
+-- Active: 1759501367036@@127.0.0.1@3306@mysql
+CREATE DATABASE IF NOT EXISTS todolist;
+
+-- Some older versions of MySQL might require you to add "WITH
+-- mysql_native_password" between "IDENTIFIED" and "BY" in the following query.
+CREATE USER IF NOT EXISTS 'todolist'@'localhost' IDENTIFIED BY 'ChrisLoicUwU';
+
+GRANT ALL PRIVILEGES ON todolist.* TO 'todolist'@'localhost';
+
+USE todolist;
+
+CREATE TABLE IF NOT EXISTS `todo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(2048) NOT NULL,
+  `done` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
